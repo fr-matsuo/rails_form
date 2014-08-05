f4cc1e8bbc326bc293d3d1755f442040e500dd38
fr-matsuo <matsuo@fact-real.com>
2014-08-05 04:13:11 +0000

    DBの非null属性を設定 #4

--- db/migrate/20140804034832_create_accounts.rb
+++ db/migrate/20140804034832_create_accounts.rb
@@ -1,13 +1,13 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :sex
-      t.string :post_first
-      t.string :post_last
-      t.integer :prefecture
-      t.string :email
+      t.string :first_name, :null => false
+      t.string :last_name, :null => false
+      t.string :sex, :null => false
+      t.string :post_first, :null => false
+      t.string :post_last, :null => false
+      t.integer :prefecture, :null => false
+      t.string :email, :null => false
       t.string :hobby
       t.text :other_hobby
       t.text :opinion
