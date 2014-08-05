9c8827769d9a27f2fb068b736f15ab9cddec08d1
fr-matsuo <matsuo@fact-real.com>
2014-08-05 04:15:48 +0000

    finish画面のhttpリクエスト調整 #4

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get  'accounts/index'
   get  'accounts/new'
   post 'accounts/check'
-  get  'accounts/finish'
+  post  'accounts/finish'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
