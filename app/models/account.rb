5908f0481d5cece10e8d9299a6ec4c17f7359103
fr-matsuo <matsuo@fact-real.com>
2014-08-07 03:49:50 +0000

    エラーメッセージを仕様に合わせて修正 #8

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,34 +1,62 @@
 class Account < ActiveRecord::Base
-  validates :first_name, :last_name,
-    presence: true,
-    length:{maximum: 50, allow_blank:true}
+  validates :first_name,
+    presence: { message: '姓を入力してください。' },
+    length:{
+      maximum: 50,
+      message: '姓は%{count}文字以内で入力してください。',
+      allow_blank:true
+    }
   
+  validates :last_name,
+    presence: { message: '名を入力してください。'},
+    length:{
+      maximum: 50,
+      message: '名は%{count}文字以内で入力してください。',
+      allow_blank:true
+    }
+
   validates :sex,
-    presence: true,
+    presence: { message: '性別を選択してください。' },
     inclusion:{in: ['男性', '女性'], allow_blank:true}
   
   validates :post_first,
-    presence: true,
-    format: {with: /\A[0-9]{3}\z/, allow_blank:true}
+    presence: { message: '郵便番号を入力してください' },
+    format: {
+      with: /\A[0-9]{3}\z/,
+      message: '郵便番号を正しく入力してください。',
+      allow_blank:true
+    }
   
   validates :post_last,
-    presence: true,
-    format: {with: /\A[0-9]{4}\z/, allow_blank:true}
+    presence: { message: '郵便番号を入力してください' },
+    format: {
+      with: /\A[0-9]{4}\z/,
+      message: '郵便番号を正しく入力してください。',
+      allow_blank:true
+    }
   
   validates :prefecture,
-    presence: true,
+    presence: { message: '都道府県を選択してください。' },
     numericality: {
       only_integer: true,
       greater_than_or_equal_to: 1,
       less_than_or_equal_to:   47,
+      message: '都道府県を選択してください。',
       allow_blank:true
     }
 
   validates :email,
-    presence: true,
-    format: {with: /\A[A-Za-z][0-9A-Za-z-_.]*@[A-Za-z][0-9A-Za-z-_.]*[A-Za-z]\z/, allow_blank:true}
+    presence: { message: 'メールアドレスを入力してください。' },
+    format: {
+      with: /\A[A-Za-z][0-9A-Za-z-_.]*@[A-Za-z][0-9A-Za-z-_.]*[A-Za-z]\z/,
+      message: 'メールアドレスを正しく入力してください。',
+      allow_blank:true
+    }
     
   validates :other_hobby,
-    presence: {if: Proc.new {|ac| ac.hobby.include?('その他')}}
+    presence: {
+      if: Proc.new {|ac| ac.hobby.include?('その他')},
+      message: 'その他の詳細を入力してください。'
+    }
     
 end
